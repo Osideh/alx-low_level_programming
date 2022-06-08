fb9b045e2b482fef43aebb5664e77973e6e5239c
Osideh <osidejoy1@gmail.com>
2022-06-08 03:51:42 -0700

    putchar with a new line

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * main - Print the word "Main"
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+		int i;
+
+			char p[] = "main";
+
+
+
+				for (i = 0; i < 9; i++)
+
+							{
+
+										_putchar(p[i]);
+
+												}
+
+						_putchar('\n');
+
+							return (0);
+
+}
