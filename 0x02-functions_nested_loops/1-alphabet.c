91f2bbfd8cd0e92e0dfaa86e03452640989cc72e
Osideh <osidejoy1@gmail.com>
2022-06-08 07:55:09 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * * print_alphabet - Print the alphabet in lowercase.
  *
@@ -12,6 +12,6 @@ void print_alphabet(void)
 int i;
 for (i = 97; i < 123; i++)
 {
-putchar(i);									}
+putchar(i);}
 putchar('\n');
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-alphabets
+ a function that prints the alphabet, in lowercase, followed by a new line
