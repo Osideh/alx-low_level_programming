def9b4a14a9808af99aa7da019312abe208cbb8c
Osideh <osidejoy1@gmail.com>
2022-06-08 05:49:03 -0700

    main.h contents

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+
+
+
+/**
+ *
+ * * main - tests function prints if integer positive or negative
+ *
+ * * Return: 0
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+		int i;
+
+
+
+			 i = 0;
+
+			 	positive_or_negative(i);
+
+
+
+					return (0);
+
+}
