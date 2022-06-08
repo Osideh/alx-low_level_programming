765b7607f5aa9b5a59f70e201d778ffa135506ed
Osideh <osidejoy1@gmail.com>
2022-06-08 00:53:48 -0700

    proogram that prints all possible different combinations of three dogitds

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,27 +16,29 @@ int main(void)
 
 {
 
-	  int tens;
+	  int hundreds;
 
-	    int ones;
+	    int tens;
+          
+              int ones;
 
-
-
-	      for (tens = 0; tens <= 9; tens++)
+	      for (hundreds = 012; hundreds <= 210; hundreds++)
 
 		          {
-
-				        for (ones = tens + 1; ones <= 9; ones++)
+			        for (tens = hundreds + 1; tens <= 210; tens++)
 
 							{
+                                {
+					for  (ones = tens + ones <= 210; ones++)
 
-									  putchar(tens + '0');
+				}
+	      			putchar(hundreds + '0');
 
-									  	  putchar(ones + '0');
+									  	  putchar(tens + '0');
 
+    putchar(ones + '0');
 
-
-										  	  if (tens < 8)
+										  	  if (hundredss < 208)
 
 												  	    {
 
