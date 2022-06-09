a001f739b1b0ff051af349e5f388d3a772f47bb0
Osideh <osidejoy1@gmail.com>
2022-06-09 06:33:20 -0700

    lowercase character

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  *
- * * print_alphabet_x10 - Prints the alphabet 10 times.
+ *print_alphabet_x10 - Prints the alphabet 10 times.
  *
- * *
  *
- * * Return: Always 0
+ *
+ *Return: Always 0
  *
  **/
 void print_alphabet_x10(void)
