75f6a0bcb5a82c0682d407c63a4214020ef87a94
Osideh <osidejoy1@gmail.com>
2022-06-15 11:07:03 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ **main - prints numbers
+ *
+ **Return: Always 0.
+ *
+ */
+void print_number(int n);
+int n = number;
+printf("Enter an integer: ");
+scanf("%d", &number);
+_putchar("You entered: %d", number);
+return (0);
+}
