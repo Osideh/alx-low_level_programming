d8bdec162c2e60afc35ed647e32eb3140f109a42
Osideh <osidejoy1@gmail.com>
2022-06-27 04:20:19 -0700

    header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifdef header_file
+#define header_file
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int num_checker(char *a);
+int main(int argc, char *argv[]);
+#endif
